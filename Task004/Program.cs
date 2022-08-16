03442f3f48af2dfe138241c561bb600d67119b00
MaxX <maxx-f@mail.ru>
2022-08-16 11:26:23 +0300

    Add Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+
+int[] ArrayCreation(int length)
+{
+    int[] arr = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+}
+PrintArray(ArrayCreation(8));
