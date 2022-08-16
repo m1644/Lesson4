9e71d6339b3c34245b1bb86fc862973b6d5028d9
MaxX <maxx-f@mail.ru>
2022-08-16 09:48:28 +0300

    Add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = (int)Math.Log10(num) + 1;
+Console.WriteLine($"Число состоит из {count} цифр.");
+
+// Решение через While
+
+int Digits (int number)
+{
+    int N = number;
+    int result = 1;
+    while (N > 10)
+    {
+      N /= 10;
+      result++;
+    }
+    return result;
+}
+void PrintDigits(int number)
+{
+    Console.WriteLine($"Число {number} содержит {Digits(number)} цифр.");
+}
+PrintDigits(4);
+PrintDigits(444);
+PrintDigits(4444);
