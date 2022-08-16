fd21690d62de9f1850d40b41a9f4d37239e26637
MaxX <maxx-f@mail.ru>
2022-08-16 08:11:38 +0300

    Add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+int Sum(int A)
+{
+    int result = 0;
+    for (int number = 0; number <= A; number++)
+    {
+        result += number; // result = result + number
+    }
+    return result;
+}
+
+void PrintSum(int A)
+{
+    Console.WriteLine($"Сумма чисел от 1 до {A} равна {Sum(A)}");
+}
+PrintSum(5);
+PrintSum(10);
+PrintSum(0);
+PrintSum(-20);
