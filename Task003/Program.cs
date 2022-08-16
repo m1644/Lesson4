4b42598635923a8b5d02e19a3fab22452d9f60b4
MaxX <maxx-f@mail.ru>
2022-08-16 10:48:42 +0300

    Add Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+
+int Count (int N)
+{
+    int result = 1;
+    for (int number = 1; number <= N; number++)
+    {
+        result *= number;
+    }
+    return result;
+}
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {Count(N)}");
